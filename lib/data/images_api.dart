4e29f3c0372343bd4721f75bbe5bf7ab5551cef3
dxxxxxkxx <dxxxxxkxx@gmail.com>
2024-01-03 08:17:14 +0900

    rename: Rename 'ImageApi' class to 'ImagesApi'

--- lib/data/images_api.dart
+++ lib/data/images_api.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-class ImageApi {
+class ImagesApi {
   Future<List<dynamic>> getImages({String query = ''}) async {
     final String url =
         'https://pixabay.com/api/?key=10711147-dc41758b93b263957026bdadb&q=$query';
