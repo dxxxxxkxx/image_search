daf7979a66803ae658d0da2dc5eaa69feef7e4a9
dxxxxxkxx <dxxxxxkxx@gmail.com>
2024-01-03 09:30:15 +0900

    feat: Create ImageItem, Modify ImageMapper to use ImageItem

---
+++ lib/data/image_item.dart
@@ -0,0 +1,5 @@
+class ImageItem {
+  final String urlSrc;
+
+  const ImageItem({required this.urlSrc});
+}
--- lib/data/image_mapper.dart
+++ lib/data/image_mapper.dart
@@ -1,9 +1,9 @@
 import 'image_dto.dart';
+import 'image_item.dart';
 
 extension ImageMapper on ImageDto {
-  ImageDto toImageDto() => ImageDto(
-      imageUrl: imageUrl ??
-          previewUrl ??
-          'https://cdn1.iconfinder.com/data/icons/carbon-design-system-vol-6/32/no-image-1024.png',
-      tags: tags ?? '');
+  ImageItem toImageItem() => ImageItem(
+      urlSrc: previewUrl ??
+          imageUrl ??
+          'https://cdn1.iconfinder.com/data/icons/carbon-design-system-vol-6/32/no-image-1024.png');
 }
