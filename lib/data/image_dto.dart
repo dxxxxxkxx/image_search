b0e020b69d92dd7918948113f811d581bdf8faa3
dxxxxxkxx <dxxxxxkxx@gmail.com>
2024-01-03 11:53:24 +0900

    변수 순서만 바뀜

--- lib/data/image_dto.dart
+++ lib/data/image_dto.dart
@@ -2,47 +2,40 @@ import 'package:flutter/material.dart';
 
 @immutable
 class ImageDto {
-  final String? imageUrl;
   final String? previewUrl;
+  final String? imageUrl;
   final String? tags;
 
-  const ImageDto({this.imageUrl, this.previewUrl, this.tags});
-
-  ImageDto.fromJson(Map<String, dynamic> json)
-      : imageUrl = json['imageURL'],
-        previewUrl = json['previewURL'],
-        tags = json['tags'];
-
-  Map<String, dynamic> toJson() => {
-        'imageURL': imageUrl,
-        'previewURL': previewUrl,
-        'tags': tags,
-      };
+  const ImageDto({this.previewUrl, this.imageUrl, this.tags});
 
   @override
   String toString() =>
-      'ImageDto{ imageUrl: $imageUrl, previewUrl: $previewUrl, tags: $tags}';
+      'ImageDto{ previewUrl: $previewUrl, imageUrl: $imageUrl, tags: $tags}';
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
       (other is ImageDto &&
           runtimeType == other.runtimeType &&
-          imageUrl == other.imageUrl &&
           previewUrl == other.previewUrl &&
+          imageUrl == other.imageUrl &&
           tags == other.tags);
 
   @override
-  int get hashCode => imageUrl.hashCode ^ previewUrl.hashCode ^ tags.hashCode;
+  int get hashCode => previewUrl.hashCode ^ imageUrl.hashCode ^ tags.hashCode;
 
-  ImageDto copyWith({
-    String? imageUrl,
-    String? previewUrl,
-    String? tags,
-  }) =>
+  ImageDto copyWith({String? previewUrl, String? imageUrl, String? tags}) =>
       ImageDto(
-        imageUrl: imageUrl ?? this.imageUrl,
         previewUrl: previewUrl ?? this.previewUrl,
+        imageUrl: imageUrl ?? this.imageUrl,
         tags: tags ?? this.tags,
       );
+
+  ImageDto.fromJson(Map<String, dynamic> json)
+      : previewUrl = json['previewURL'],
+        imageUrl = json['imageURL'],
+        tags = json['tags'];
+
+  Map<String, dynamic> toJson() =>
+      {'previewURL': previewUrl, 'imageURL': imageUrl, 'tags': tags};
 }
