6c0b21e15f32e27b8078af9c12da8ee942e53375
dxxxxxkxx <dxxxxxkxx@gmail.com>
2024-01-03 09:28:52 +0900

    comment: Remove default comment

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ void main() {
 class ImageSearch extends StatelessWidget {
   const ImageSearch({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
